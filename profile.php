a6da86bf78640a46005b0fd9c5030e722e84dfc1
hhp46 <hhp46@njit.edu>
2020-10-20 21:31:52 -0400

    project files

--- profile.php
+++ profile.php
@@ -1,3 +1,4 @@
+<title> Profile Page </title>
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <?php
 //Note: we have this up here, so our update happens before our get/fetch
@@ -64,8 +65,6 @@ if (isset($_POST["saved"])) {
         }
     }
     if ($isValid) {
-        $userID = null;
-        $currentPass = null;
         $stmt = $db->prepare("UPDATE Users set email = :email, username= :username where id = :id");
         $r = $stmt->execute([":email" => $newEmail, ":username" => $newUsername, ":id" => get_user_id()]);
         if ($r) {
@@ -76,34 +75,21 @@ if (isset($_POST["saved"])) {
         }
         //password is optional, so check if it's even set
         //if so, then check if it's a valid reset request
-        if (!empty($_POST["newPassword"]) && !empty($_POST["confirm"]) && !empty($_POST["password"])) {
-            $currentPass = $_POST["password"];
-            $stmt = $db->prepare("SELECT password from Users WHERE id = :id");
-            $params = array(":id" => get_user_id());
-            $r = $stmt->execute($params);
-           $e = $stmt->errorInfo();
-            if ($e[0] != "00000") {
-                flash("Something went wrong, please try again");  }
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result && isset($result["password"])) {
-                
-                $DBPassHash = $result["password"];
-            if (password_verify($currentPass, $DBPassHash)) {
-                if ($_POST["newPassword"] == $_POST["confirm"]) {
-                    $newPassword = $_POST["newPassword"];
-                    $newPassHash = password_hash($newPassword, PASSWORD_BCRYPT);
-                    //this one we'll do separate
-                    $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
-                    $r = $stmt->execute([":id" => get_user_id(), ":password" => $newPassHash]);
-                
-                    if ($r) {
-                        flash("Reset Password"); }
-                    else {
-                        flash("Error resetting password"); }
+        if (!empty($_POST["password"]) && !empty($_POST["confirm"])) {
+            if ($_POST["password"] == $_POST["confirm"]) {
+                $password = $_POST["password"];
+                $hash = password_hash($password, PASSWORD_BCRYPT);
+                //this one we'll do separate
+                $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
+                $r = $stmt->execute([":id" => get_user_id(), ":password" => $hash]);
+                if ($r) {
+                    flash("Reset Password");
+                }
+                else {
+                    flash("Error resetting password");
+                }
             }
-            }   
         }
-    } 
 //fetch/select fresh data in case anything changed
         $stmt = $db->prepare("SELECT email, username from Users WHERE id = :id LIMIT 1");
         $stmt->execute([":id" => get_user_id()]);
@@ -119,21 +105,78 @@ if (isset($_POST["saved"])) {
     else {
         //else for $isValid, though don't need to put anything here since the specific failure will output the message
     }
+    
+   
+   
+    if ($isValid) {
+        $db = getDB();
+        if (isset($db)) {
+            $stmt = $db->prepare("SELECT password from Users WHERE id = :id");
+
+            $params = array(":id" => $id);
+            $r = $stmt->execute($params);
+            //echo "db returned: " . var_export($r, true);
+            $e = $stmt->errorInfo();
+           
+        
+           
+            
+          
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result && isset($result["password"])) {
+                $password_hash_from_db = $result["password"];
+                if (password_verify($password, $password_hash_from_db)) {
+                    $stmt = $db->prepare("
+SELECT Roles.name FROM Roles JOIN UserRoles on Roles.id = UserRoles.role_id where UserRoles.user_id = :user_id and Roles.is_active = 1 and UserRoles.is_active = 1");
+                    $stmt->execute([":user_id" => $result["id"]]);
+                    $roles = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+                    unset($result["password"]);//remove password so we don't leak it beyond this page
+                    //let's create a session for our user based on the other data we pulled from the table
+                    $_SESSION["user"] = $result;//we can save the entire result array since we removed password
+                    if ($roles) {
+                        $_SESSION["user"]["roles"] = $roles;
+                    }
+                    else {
+                        $_SESSION["user"]["roles"] = [];
+                    }
+                    //on successful login let's serve-side redirect the user to the home page.
+                    flash("Updated Profile");
+                    die(header("Location: profile.php"));
+                }
+                else {
+                    flash("Old password invalid");
+                }
+            }
+            else {
+                flash("Passwords don't match");
+            }
+        }
+    }
+    else {
+        flash("There was a validation issue");
+    }
+   
+   
 }
 
 
 ?>
+<br>
 
+<p3>Profile:</p3>
+<br>
+<br>
     <form method="POST">
         <label for="email">Email</label>
         <input type="email" name="email" value="<?php safer_echo(get_email()); ?>"/>
         <label for="username">Username</label>
         <input type="text" maxlength="60" name="username" value="<?php safer_echo(get_username()); ?>"/>
         <!-- DO NOT PRELOAD PASSWORD-->
+          <label for="currentpass">Current Password</label>
+        <input type="password" name="current"/>
         <label for="pw">Password</label>
-        <input type="password" name="password"/>
-        <label for="npw">New Password</label>
-        <input type="password" name="newPassword"/>
+        <input type="password" name="npassword"/>
         <label for="cpw">Confirm Password</label>
         <input type="password" name="confirm"/>
         <input type="submit" name="saved" value="Save Profile"/>
