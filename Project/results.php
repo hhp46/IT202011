3c74742b864cd9bccb99d7590686ee969f5198fc
hhp46 <hhp46@njit.edu>
2020-11-20 21:35:47 -0500

    project files

--- Project/results.php
+++ Project/results.php
@@ -19,30 +19,63 @@ if ($r) {
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
 }
 else {
-    flash("There was a problem fetching responses: " . var_export($stmt->errorInfo(), true));
+    flash("There was a problem fetching survey detail: " . var_export($stmt->errorInfo(), true));
 }
 }
 ?>
 
+<?php
+if (isset($_GET["id"])) {
+    $sid = $_GET["id"];
+$db = getDB();
+$stmt = $db->prepare("SELECT user_id, survey_id, question_id, answer_id FROM Responses JOIN Answers on Answers.id = Responses.answer_id JOIN Questions on Responses.question_id = Questions.id WHERE Responses.survey_id = :survey and Responses.user_id = :user");
+
+$r = $stmt->execute([":id" => get_user_id(),":survey_id" => $sid]);
+if ($r) {
+    $results = $stmt->fetchALL(PDO::FETCH_ASSOC);
+}
+else {
+    flash("There was a problem fetching responses: " . var_export($stmt->errorInfo(), true));
+}
+}
+
+?>
 
 
 <h3>Your Survey Responses</h3>
 
         
  <?php if (isset($result) && !empty($result)): ?>
-    <div class="card">
-     
+      <div class="results">   
         <div class="card-body">
             <div>
                 
                 <div>Survey Title: <?php safer_echo($result["title"]); ?></div>
                 <div>Description:: <?php safer_echo($result["description"]); ?></div>
-              
-            </div>
+      
+      
+       <?php if (count($results) > 0): ?>
+        
+            <?php foreach ($results as $r): ?>
+                
+                    <div>
+                        <div>Question: <?php safer_echo($r["question"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Answer: <?php safer_echo($r["answer"]); ?></div>
+                    </div>
+                    </div>
+             <?php endforeach; ?>
         </div>
-    </div>
-<?php else: ?>
-    <p>Error ...</p>
-<?php endif; ?>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>
+       
 
+            
+     
+    
+    
+    
 <?php require(__DIR__ . "/partials/flash.php");
